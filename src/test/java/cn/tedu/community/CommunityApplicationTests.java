57fd45e5fd355e96be913f5980a49871b3aed10f
= <=>
2020-03-21 16:27:55 +0800

    init repo

---
+++ src/test/java/cn/tedu/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
