6a2a7edfbe39b946bf7063bb341ff6f03de69a83
= <=>
2020-03-24 23:01:51 +0800

    extract githup config from code to properties

--- src/main/java/cn/tedu/community/controller/AuthorizeController.java
+++ src/main/java/cn/tedu/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import cn.tedu.community.dto.AccessTokenDTO;
 import cn.tedu.community.dto.GitupUser;
 import cn.tedu.community.provider.GitupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,16 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GitupProvider gitupProvider;
 
+    @Value("${githup.client.id}")
+    private String clientId;
+    @Value("${githup.client.secret}")
+    private String clientSecret;
+    @Value("${githup.redirect.uri}")
+    private String redirectUri;
+
 
     @RequestMapping("/callback")
     public String callback(@RequestParam("code")String code,
                            @RequestParam("state")String state){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("c88e822b0d957ee2db35");
-        accessTokenDTO.setClient_secret("78e5e7777eda49cb051555928c8669f7923d4457");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken=gitupProvider.getAccessToken(accessTokenDTO);
         GitupUser user=gitupProvider.getGitupUser(accessToken);
         System.out.println(user.getName());
